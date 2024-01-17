8e4fbc7be44bd2721062c5e4771ff349d2328afa
balagrivine <balagrivine@gmail.com>
2024-01-17 12:25:32 -0800

    solve the rendering error where template with post method was not being rendered

--- pycasts/app.py
+++ pycasts/app.py
@@ -5,24 +5,20 @@ from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def greet_user():
-    return "<p>Hello User</p>"
-
 @app.route("/<name>/<username>")
 def username(name, username):
     return ("Hello {} {}".format(escape(username), escape(name)))
-@app.route('/login', methods=['GET', 'POST'])
+
+def login_post():
+    return render_template('app.html')
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return render_template('app.html')
-    else:
-        return login_get()
-@app.get('/login')
+        return login_post()
+    return login_get()
+
 def login_get():
-    return "Get method used"
-@app.post('/login')
-def login_post():
-    return "POST method used"
+    return render_template('app.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
