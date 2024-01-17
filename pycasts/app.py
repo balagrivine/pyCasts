705c211bd4e00989acdb979ff416d9488288286c
balagrivine <balagrivine@gmail.com>
2024-01-17 00:06:51 -0800

    update the app to manage urls effectively

--- pycasts/app.py
+++ pycasts/app.py
@@ -5,25 +5,24 @@ from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
-"""decorator to direct Flask which URL to display for the function"""
-@app.route("/<name>")
-def admin_world(name):
-    return ("Hello {}".format(escape(name)))
-
 @app.route("/")
 def greet_user():
     return "<p>Hello User</p>"
 
-@app.route("/name/<username>")
-def username(username):
-    return ("Hello {}".format(escape(username)))
+@app.route("/<name>/<username>")
+def username(name, username):
+    return ("Hello {} {}".format(escape(username), escape(name)))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return "POST method used"
+        return login_post()
     else:
-        return "GET method used"
-
-
+        return login_get()
+@app.get('/login')
+def login_get():
+    return "Get method used"
+@app.post('/login')
+def login_post():
+    return "POST method used"
 if __name__ == "__main__":
     app.run(debug=True)
